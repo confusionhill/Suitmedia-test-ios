bafec8299a825c7a68271965df46bac2592179f3
farhan <18220015@std.stei.itb.ac.id>
2022-10-18 18:20:23 +0700

    change bar color

--- SIUUUU/Presentation/BaseViewController.swift
+++ SIUUUU/Presentation/BaseViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.tintColor = .purple
+        navigationController?.navigationBar.tintColor = UIColor(red: 0.33, green: 0.29, blue: 0.94, alpha: 1.00)
         self.navigationItem.setHidesBackButton(true, animated: true)
-        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(didTapBackButton))
+        let backButton = UIBarButtonItem(
+            image: UIImage(systemName: "chevron.backward"),
+            style: .plain,
+            target: self,
+            action: #selector(didTapBackButton))
         self.navigationItem.leftBarButtonItem = backButton
     }
     override func viewWillAppear(_ animated: Bool) {
